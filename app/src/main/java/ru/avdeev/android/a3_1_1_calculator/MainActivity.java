9e51558da7bad9d9963f16b178fcbe3bb14a382b
Avdeev Aleksey <avdeev.hexer@yandex.ru>
2020-09-17 13:42:01 +0300

    Implemented using the anyButtonClicked(View view) method

--- app/src/main/java/ru/avdeev/android/a3_1_1_calculator/MainActivity.java
+++ app/src/main/java/ru/avdeev/android/a3_1_1_calculator/MainActivity.java
@@ -2,7 +2,9 @@ package ru.avdeev.android.a3_1_1_calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -20,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     private Button buttonEight;
     private Button buttonNine;
     private Button buttonPoint;
+    private View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,5 +44,59 @@ public class MainActivity extends AppCompatActivity {
         buttonEight = findViewById(R.id.btnEight);
         buttonNine = findViewById(R.id.btnNine);
         buttonPoint = findViewById(R.id.btnPoint);
+
     }
+
+    public void anyButtonClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btnZero: {
+                textViewResult.setText(buttonZero.getText().toString());
+                break;
+            }
+            case R.id.btnOne: {
+                textViewResult.setText(buttonOne.getText().toString());
+                break;
+            }
+            case R.id.btnTwo: {
+                textViewResult.setText(buttonTwo.getText().toString());
+                break;
+            }
+
+            case R.id.btnThree: {
+                textViewResult.setText(buttonThree.getText().toString());
+                break;
+            }
+            case R.id.btnFore: {
+                textViewResult.setText(buttonFore.getText().toString());
+                break;
+            }
+            case R.id.btnFive: {
+                textViewResult.setText(buttonFive.getText().toString());
+                break;
+            }
+            case R.id.btnSix: {
+                textViewResult.setText(buttonSix.getText().toString());
+                break;
+            }
+            case R.id.btnSeven: {
+                textViewResult.setText(buttonSeven.getText().toString());
+                break;
+            }
+            case R.id.btnEight: {
+                textViewResult.setText(buttonEight.getText().toString());
+                break;
+            }
+            case R.id.btnNine: {
+                textViewResult.setText(buttonNine.getText().toString());
+                break;
+            }
+            case R.id.btnPoint: {
+                textViewResult.setText(buttonPoint.getText().toString());
+                break;
+            }
+        }
+
+    }
+
+
 }
